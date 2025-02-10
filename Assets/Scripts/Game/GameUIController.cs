81981bbb49ded050a615eb5e326e7fa4c5ab752e
Junil Ko <goyadb@outlook.com>
2025-02-10 11:45:02 +0900

    Navigation Controller 구현

--- Assets/Scripts/Game/GameUIController.cs
+++ Assets/Scripts/Game/GameUIController.cs
@@ -63,4 +63,17 @@ public class GameUIController : MonoBehaviour
             GameManager.Instance.ChangeToMainScene();
         });
     }
+
+    public void OnClickSettingsButton()
+    {
+        GameManager.Instance.OpenSettingsPanel();
+    }
+
+    public void OnClickBackButton()
+    {
+        GameManager.Instance.OpenConfirmPanel("게임을 종료하시겠습니까?", () =>
+        {
+            GameManager.Instance.ChangeToMainScene();
+        });
+    }
 }
