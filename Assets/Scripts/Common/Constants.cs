62cbc6064f124c90bb1bf990cc0884d165192564
Junil Ko <junil@mocapot.com>
2025-03-07 11:28:54 +0900

    채팅 예제 구현

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -1,4 +1,13 @@
 public class Constants
 {
     public const string ServerURL = "http://localhost:3000";
+    public const string GameServerURL = "ws://localhost:3000";
+
+    public enum MultiplayManagerState
+    {
+        CreateRoom,
+        JoinRoom,
+        StartGame,
+        EndGame
+    };
 }
---
+++ Assets/Scripts/Common/MultiplayManager.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using SocketIOClient;
+using UnityEngine.Experimental.Audio;
+
+public class RoomData
+{
+    [JsonProperty("roomId")]
+    public string roomId { get; set; }
+}
+
+public class UserData
+{
+    [JsonProperty("userId")]
+    public string userId { get; set; }
+}
+
+public class MessageData
+{
+    [JsonProperty("nickName")]
+    public string nickName { get; set; }
+    [JsonProperty("message")]
+    public string message { get; set; }
+}
+
+public class MultiplayManager
+{
+    private SocketIOUnity _socket;
+    private event Action<Constants.MultiplayManagerState, string> _onMultiplayStateChanged;
+    public Action<MessageData> OnReceiveMessage;
+    
+    public MultiplayManager(Action<Constants.MultiplayManagerState, string> onMultiplayStateChanged)
+    {
+        _onMultiplayStateChanged = onMultiplayStateChanged;
+        
+        var uri = new Uri(Constants.GameServerURL);
+        _socket = new SocketIOUnity(uri, new SocketIOOptions
+        {
+            Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+        });
+        
+        _socket.On("createRoom", CreateRoom);
+        _socket.On("joinRoom", JoinRoom);
+        _socket.On("startGame", StartGame);
+        _socket.On("gameEnded", GameEnded);
+        _socket.On("receiveMessage", ReceiveMessage);
+        
+        _socket.Connect();
+    }
+
+    private void CreateRoom(SocketIOResponse response)
+    {
+        var data = response.GetValue<RoomData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.CreateRoom, data.roomId);
+    }
+
+    private void JoinRoom(SocketIOResponse response)
+    {
+        var data = response.GetValue<RoomData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.JoinRoom, data.roomId);
+    }
+
+    private void StartGame(SocketIOResponse response)
+    {
+        var data = response.GetValue<UserData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.StartGame, data.userId);
+    }
+
+    private void GameEnded(SocketIOResponse response)
+    {
+        var data = response.GetValue<UserData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.EndGame, data.userId);
+    }
+
+    private void ReceiveMessage(SocketIOResponse response)
+    {
+        var data = response.GetValue<MessageData>();
+        OnReceiveMessage?.Invoke(data);
+    }
+
+    public void SendMessage(string roomId, string nickName, string message)
+    {
+        _socket.Emit("sendMessage", new { roomId, nickName, message });
+    }
+}
